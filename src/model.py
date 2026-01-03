679a9a8605a2d08e45cc79bd9e8221692ab9d0f9
Emil Ötting <emil.oetting@gmx.de>
2026-01-03 11:55:28 +0100

    Import default clap for evalutation

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ import os
 from transformers import AutoModel
 from peft import PeftModel, PeftConfig
 from transformers import ClapProcessor
-
+from laion_clap import CLAP_Module
 
 # Check whether config is available
 cwd = os.getcwd()
@@ -17,8 +17,11 @@ config = PeftConfig.from_pretrained(model_dir)
 model = AutoModel.from_pretrained(config.base_model_name_or_path)
 
 MODEL = PeftModel.from_pretrained(model, model_dir)
+UNTRAINED_MODEL = CLAP_Module(enable_fusion=False)
+UNTRAINED_MODEL.load_ckpt(verbose=False)
 PROCESSOR = ClapProcessor.from_pretrained("laion/clap-htsat-unfused")
 TOKENIZER = PROCESSOR.tokenizer
 FEAT_EXTR = PROCESSOR.feature_extractor
 TARGET_SR = 48000
 TARGET_AUDIO_LEN_SEC = 5   # FML
+
